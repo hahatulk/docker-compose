6337a07f1eb3fe18fbd58632afafe2cad43421f0
Vadim <goljatkin2128506@gmail.com>
2026-04-08 10:08:23 +0300

    upd

--- zrok/up.sh
+++ zrok/up.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/hahatulk/zrok.git
+cp .env ./zrok/docker/compose/zrok2-instance/.env
 cd ./zrok/docker/compose/zrok2-instance/
 docker compose up -d
 sudo ufw-docker reload
