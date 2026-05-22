e670767fd82ae3a5c19215857a379e6d2b9ab2c2
Vadim <goljatkin2128506@gmail.com>
2026-05-22 22:09:46 +0300

    upd

---
+++ obsidian/setup.sh
@@ -0,0 +1,6 @@
+export hostname=https://domain.ru
+export database=my-vault
+export passphrase=any_pass # change to whatever that suits u
+export username=any_username     # change to whatever that suits u
+export password=any_pass2     # change to whatever that suits u
+deno run -A https://raw.githubusercontent.com/vrtmrz/obsidian-livesync/main/utils/flyio/generate_setupuri.ts
